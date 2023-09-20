1cd795bc0fafa74bc833a93f75205987ee679245
JulioMoudatsos <juliomoud@gmail.com>
2023-09-20 15:02:47 -0300

    first commit

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,56 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTest {
+
+    @BeforeClass
+    public static void init(){
+        RestAssured.baseURI ="http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void tarefasListadas(){
+        given().when()
+                .get("/todo")
+                .then().statusCode(HttpStatus.SC_OK);
+    }
+
+
+    @Test
+    public void publicarTarefa(){
+
+        given().
+                contentType(ContentType.JSON).
+                body("   {\n" +
+                        "\n" +
+                        "            \"task\": \"julio2\",\n" +
+                        "                \"dueDate\": \"2023-09-21\"\n" +
+                        "        }")
+                .when()
+                .post("/todo")
+                .then().statusCode(HttpStatus.SC_CREATED);
+
+    }
+
+
+    @Test
+    public void naoPublicarTarefa(){
+
+        given().
+                contentType(ContentType.JSON).
+                body("   {\n" +
+                        "\n" +
+                        "            \"task\": \"julio2\",\n" +
+                        "                \"dueDate\": \"2020-09-21\"\n" +
+                        "        }")
+                .when()
+                .post("/todo")
+                .then().statusCode(400);
+
+    }
+}
